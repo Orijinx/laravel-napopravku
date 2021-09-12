a49052c42dd821dba4799df39dc66d4ab8e5bea9
Vlad <vladonnx@mail.ru>
2021-09-12 04:47:12 +0300

    Add ru lang

--- app/Http/Controllers/API/BaseAPIController.php
+++ app/Http/Controllers/API/BaseAPIController.php
@@ -19,7 +19,7 @@ class BaseAPIController extends Controller
     public function Error($error,string $msg){
         $response = [
             'status'=>false,
-            'errors'=>$error,
+            'errors'=>[$error],
             'msg'=>$msg
         ];
         return response()->json($response);
--- app/Http/Controllers/API/PostQuoteController.php
+++ app/Http/Controllers/API/PostQuoteController.php
@@ -9,30 +9,31 @@ use App\Models\quote;
 
 class PostQuoteController extends BaseAPIController
 {
-    //
+    //Добавление записи цитаты в БД.
     public function PostQuote(Request $request)
     {
+        //Объявление валидатора и определение Rules для валидации реквеста.
         $validator = Validator::make(
             $request->all(),
             [
                 'text' => ['required'],
                 'author' => ['required', 'max:255'],
-                'tags' => ['array','exists:App\Models\tag,id']
+                'tags' => ['required', 'array', 'exists:App\Models\tag,id']
             ]
         );
+        //Проверка ошибок валидации
         if ($validator->fails()) {
             return $this->Error($validator->errors(), 'Ошибка валидации');
-
         }
+        //Создание новой записи цитаты
         $quote = new quote();
         $quote->text = $request->text;
         $quote->author = $request->author;
         $quote->save();
 
-        if(isset($request->tags)){
-            $quote->Tags()->attach($request->tags);
-        }
-        
+        //Прикрепление тэгов к цитате
+        $quote->Tags()->attach($request->tags);
+
         return $this->Success($quote, 'Цитата успешно добавлена!');
     }
 }
