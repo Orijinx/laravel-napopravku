3030dbabfe4ead6154cd3b026ddd3f3718a0eb57
Vlad <vladonnx@mail.ru>
2021-09-13 16:57:09 +0300

    Merge Back-end and Front-end

---
+++ resources/views/vue-app/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href="{{asset('/favicon.png')}}" />
+        <title>Quotes</title>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css"
+        />
+        <link href="{{asset('/js/about.34fc5cfc.js')}}" rel="prefetch" />
+        <link href="{{asset('/css/chunk-vendors.526f04ed.css')}}" rel="preload" as="style" />
+        <link href="{{asset('/js/app.e113ec89.js" rel="preload')}}" as="script" />
+        <link href="{{asset('/js/chunk-vendors.2e5e4c47.js')}}" rel="preload" as="script" />
+        <link href="{{asset('/css/chunk-vendors.526f04ed.css')}}" rel="stylesheet" />
+    </head>
+    <body>
+        <noscript
+            ><strong
+                >We're sorry but vue-vuetify doesn't work properly without
+                JavaScript enabled. Please enable it to continue.</strong
+            ></noscript
+        >
+        <div id="app"></div>
+        <script src="{{asset('/js/chunk-vendors.2e5e4c47.js')}}"></script>
+        <script src="{{asset('/js/app.e113ec89.js')}}"></script>
+    </body>
+</html>
